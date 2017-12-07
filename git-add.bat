b6e7de29ddddc79ee3d0255a18736d60b82328d2
fucan123 <120280217@qq.com>
2017-12-07 15:01:58 +0800

    提交

--- git-add.bat
+++ git-add.bat
@@ -2,7 +2,7 @@ cd %~d0
 git add .
 git commit -m "Ã·Ωª"
 git remote add origin https://github.com/fucan123/fish-cpp.git
-git pull origin fish-cpp
-git push -u origin fish-cpp
+git pull
+git push -u origin
 
 
